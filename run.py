35e380fd6cdcd0dd23268877f8159f109697aef9
lesreddy <les.reddy@gmail.com>
2019-11-25 11:18:46 +0000

    connected mongodb to flask app

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'Terrie_Homes'
+app.config["MONGO_URI"] = 'mongodb+srv://Redler:LakeBoga29@myfirstcluster-rvlcz.mongodb.net/Terrie_Homes?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
@@ -17,7 +24,7 @@ def contact():
 
 @app.route("/listings")
 def listings():
-    return render_template("listings.html")
+    return render_template("listings.html", for_sale=mongo.db.for_sale.find())
 
 if __name__== "__main__":
     app.run(host=os.environ.get("IP"),
